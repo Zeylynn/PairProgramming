c9f3728f36a9d572677e29c01c31c65933789c17
Vivi <suremymail@example.com>
2025-03-25 09:19:27 +0100

    Platform Input geht!

--- API.py
+++ API.py
@@ -1,17 +1,34 @@
+# https://github.com/Zeylynn/PairProgramming.git
+
 import requests as req
 
-def NetGagget(url):
-    data = req.get(url).json()
-    return data
+def getAPI(url):
+    return req.get(url).json()
 
 url = "https://www.gamerpower.com/api/giveaways"
 
-Dictionary = NetGagget(url)
+data = getAPI(url)
 
-lauf = 0
-for i in Dictionary:
-    print("Titel:",(i["title"]),"URL:",i["open_giveaway_url"],"\n")
-    lauf+=1
-    
-print("")
-print(lauf)
+# Platform Input
+while(True):
+    try:
+        platform = input("Auf welcher Platform sollen die Spiele gespielt werden\n1) PC\n2) Android\n3) XBox\n4) Playstation\n5) Nintendo Switch\n")
+
+        match platform:
+            case "1": platform = "PC"; break
+            case "2": platform = "Android"; break
+            case "3": platform = "Xbox"; break
+            case "4": platform = "Playstation"; break
+            case "5": platform = "Switch"; break
+    except KeyboardInterrupt:
+        break
+    except:
+        pass
+
+j = 0
+for i in data:
+    if platform in i["platforms"]:
+        print("Titel:",(i["title"]),"URL:",i["open_giveaway_url"],"\n")
+        j += 1
+
+print(f"Anzahl der gefundenen Beiträge: {j}")
